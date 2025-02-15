62b11a81d52b41fe56d5cbd0af178159ef5f91aa
chukwu emmanuel onyedikachi <onyiemmanuel75@gmail.com>
2025-02-15 09:29:08 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import React, { useState } from "react";
-import MovieDivs from "./blogPost"; // Your movie component
-import Pagination from "@mui/material/Pagination";
-import movies from "./movieData"; // Import the static movie data
-import Search from "./Search_section";
-import ScrollToTop from "./scrollto";
+import React from "react";
 import "./style.css";
 import NavBar from "./components/NavBar";
 import LandingPage from "./LandingPage.jsx";
