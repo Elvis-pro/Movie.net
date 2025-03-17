17158a1bcc8b2465dcc21ffc5372639e92fb3cfa
ELVIS DAVID <94135572+Elvis-pro@users.noreply.github.com>
2025-03-17 00:46:46 +0000

    updated

--- src/components/AdsDivs.js
+++ src/components/AdsDivs.js
@@ -6,7 +6,13 @@ export default function AdsDivs() {
     <div className="adsdivs">
       <div style={{ display: "flex" }}>
         <div className="b1"></div>
-        <div className="b2"></div>
+        <div className="b2">
+          <div className="inner-adsdiv"></div>
+        </div>
+      </div>
+      <div className="AdsDivs2">
+        <div className="s-b1"></div>
+        <div className="s-b2"></div>
       </div>
     </div>
   );
