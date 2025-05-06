c8b6b379a5f1977f1b93a8d946c767e0c556c2c3
ELVIS DAVID <94135572+Elvis-pro@users.noreply.github.com>
2025-05-06 23:08:59 +0000

    updated some webpages

--- src/components/SeriesPost.js
+++ src/components/SeriesPost.js
@@ -1,16 +1,16 @@
 import { useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams, Link } from "react-router-dom";
 import series from "../SeriesData";
 
 export default function MovieGrid() {
   const navigate = useNavigate();
   const { page } = useParams(); // Get the page number from the URL
-  const currentPage = parseInt(page) || 1; // Default to page 1
+  const currentPage = parseInt(page) || 1;
   const moviesPerPage = 50;
   const visibleMovies = currentPage * moviesPerPage;
 
   const loadMoreMovies = () => {
-    navigate(`/TVSERIESLIST/${currentPage + 1}`); // Update URL dynamically
+    navigate(`/TVSERIESLIST/${currentPage + 1}`);
   };
 
   return (
@@ -25,31 +25,36 @@ export default function MovieGrid() {
         }}
       >
         {series.slice(0, visibleMovies).map((show, index) => (
-          <div
+          <Link
+            to={`/series/${show.slug}`}
             key={index}
-            className="movie-card"
-            style={{
-              border: "1px solid #cccccc",
-              borderRadius: "5px",
-              textAlign: "center",
-            }}
+            style={{ textDecoration: "none", color: "inherit" }}
           >
             <div
-              className="movie-image"
+              className="movie-card"
               style={{
-                backgroundImage: `url(${show.image})`,
-                height: "250px",
-                backgroundSize: "cover",
-                backgroundPosition: "center",
+                border: "1px solid #cccccc",
                 borderRadius: "5px",
+                textAlign: "center",
               }}
-            ></div>
-            <div className="movie-info">
-              <h3 style={{ fontSize: "12px", margin: "10px 0" }}>
-                {show.title}
-              </h3>
+            >
+              <div
+                className="movie-image"
+                style={{
+                  backgroundImage: `url(${show.image})`,
+                  height: "250px",
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  borderRadius: "5px",
+                }}
+              ></div>
+              <div className="movie-info">
+                <h3 style={{ fontSize: "12px", margin: "10px 0" }}>
+                  {show.title}
+                </h3>
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
       {visibleMovies < series.length && (
